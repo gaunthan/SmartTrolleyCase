afeb6ace30d6d5a4cf762077f20176b6faea29ca
gaunthan <gaunthanHuang@foxmail.com>
2016-05-24 17:18:32 +0800

    整合代码到TI开发板，封装地理位置获取与发送功能

---
+++ Debug/general.pp
@@ -0,0 +1,11 @@
+# FIXED
+
+general.obj: ../general.c
+general.obj: ../general.h
+general.obj: D:/Program\ Files/TI/ccsv6/tools/compiler/msp430_4.3.3/include/stdlib.h
+general.obj: D:/Program\ Files/TI/ccsv6/tools/compiler/msp430_4.3.3/include/linkage.h
+
+../general.c: 
+../general.h: 
+D:/Program\ Files/TI/ccsv6/tools/compiler/msp430_4.3.3/include/stdlib.h: 
+D:/Program\ Files/TI/ccsv6/tools/compiler/msp430_4.3.3/include/linkage.h: 
